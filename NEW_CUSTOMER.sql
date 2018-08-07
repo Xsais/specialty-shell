3feee7352e2a79aed6cba450d65846cb44f77736
Nathaniel Primo <nathaniel.primo@gmail.com>
2018-08-07 18:31:36 -0400

    Added error reporting

---
+++ NEW_CUSTOMER.sql
@@ -0,0 +1,66 @@
+CREATE OR replace FUNCTION NEW_CUSTOMER (
+	cname  customer.cname%TYPE,
+	street customer.cstreet%TYPE,
+	city   customer.ccity%TYPE,
+	prov   customer.cprov%TYPE,
+	postal customer.cpostal%TYPE,
+	hphone customer.chphone%TYPE,
+	bphone customer.cbphone%TYPE
+)
+RETURN SMALLINT
+AS
+BEGIN
+
+    IF NOT REGEXP_LIKE(postal, '(\d{5}([ \-]\d{4})?)|([ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1}[ \-]\d{1}[A-Z]{1}\d{1})')
+        THEN
+        
+            RETURN -3;
+    END IF;
+
+    IF NOT REGEXP_LIKE(hphone, '\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*')
+        THEN
+        
+            RETURN -4;
+    END IF;
+    
+    IF NOT REGEXP_LIKE(bphone, '\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*')
+        THEN
+        
+            RETURN -5;
+    END IF;
+
+    INSERT INTO customer
+    	(
+    	   cname,
+    	   cstreet,
+    	   ccity,
+    	   cprov,
+    	   cpostal,
+    	   chphone,
+    	   cbphone
+    	)
+    VALUES (
+       cname,
+       street,
+       city,
+       prov,
+       postal,
+       hphone,
+       bphone
+     );
+
+    RETURN 0;
+EXCEPTION
+
+    WHEN DUP_VAL_ON_INDEX THEN
+             RETURN -2;
+
+    WHEN OTHERS THEN
+             RETURN -1;
+END;
+/
+
+/** OUTPUT:
+ * Function NEW_CUSTOMER compiled
+ *
+ **/
