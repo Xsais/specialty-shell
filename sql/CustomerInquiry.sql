4050c186e85926cb95ce037a860266e97a844b13
Bojoofu <graujsheridan@gmail.com>
2018-08-07 17:28:55 -0400

    Type Correction

--- sql/CustomerInquiry.sql
+++ sql/CustomerInquiry.sql
@@ -18,6 +18,6 @@ BEGIN
 	DBMS_OUTPUT.PUT_LINE('------------------------------------------------------------' || CHR(10) || CHR(9) || CHR(9) || CHR(9) || TRIM(v_customer.cname) || ' Customer Information' || CHR(10) || '------------------------------------------------------------' || CHR(10) || 'Address: ' || TRIM(v_customer.cstreet) || ', ' || TRIM(v_customer.ccity) || ', ' || TRIM(v_customer.cprov) || ' ' || TRIM(v_customer.cpostal) || CHR(10) || 'Home: ' || TRIM(v_customer.chphone) || CHR(10) || 'Work: ' || TRIM(v_customer.cbphone));
 EXCEPTION
 	WHEN NO_DATA_FOUND THEN
-		DBMS_OUTPUT.PUT_LINE('Oops...  That customer cannot be found.  PLease try again.');
+		DBMS_OUTPUT.PUT_LINE('Oops...  That customer cannot be found.  Please try again.');
 END;
 /
