5c0b236f7ab71b4031abc629217a827c9b756e65
Bojoofu <graujsheridan@gmail.com>
2018-08-07 20:52:39 -0400

    Fixed Duplicate

--- sql/viewProspect.sql
+++ sql/viewProspect.sql
@@ -1,24 +1,35 @@
+-- Prompt the user for a customer name
 ACCEPT p_customerName PROMPT 'Enter a Prospective Customers Name: '
 
 DECLARE
 	-- Create needed variables to hold rows from tables and count of items
-	v_prospect prospect%ROWTYPE;
+	v_prospectCName prospect.cname%TYPE;
+	v_prospectMake prospect.make%TYPE;
+	v_prospectModel prospect.model%TYPE;
+	v_prospectCYear prospect.cyear%TYPE;
+	v_prospectColor prospect.color%TYPE;
+	v_prospectTrim prospect.trim%TYPE;
 	v_prospectOption options%ROWTYPE;
 	v_prospectCount NUMBER;
 
 	-- Cursor to store the prospect entries in the DB
 	CURSOR prospectiveCustomers_cur IS 
-	SELECT *
-	  FROM prospect
-	 WHERE TRIM(UPPER(cname)) = TRIM(UPPER('&p_customerName'));
+		SELECT *
+		  FROM prospectCustomerCars
+		 WHERE TRIM(UPPER(cname)) = TRIM(UPPER('&p_customerName'));
 
 	-- Cursor to store the order options for the prospective entries
 	CURSOR prospectCustomersOptions_cur IS 
-	SELECT o.*
-	  FROM prospect p
-	 INNER JOIN options o
-		ON p.ocode = o.ocode
-	 WHERE TRIM(UPPER(p.cname)) = TRIM(UPPER('&p_customerName'));
+		SELECT o.*
+		  FROM prospect p
+		 INNER JOIN options o
+			ON p.ocode = o.ocode
+		 WHERE TRIM(UPPER(p.cname)) = TRIM(UPPER('&p_customerName'))
+		   AND TRIM(UPPER(make)) = TRIM(UPPER(v_prospectMake))
+		   AND TRIM(UPPER(model)) = TRIM(UPPER(v_prospectModel))
+		   AND TRIM(UPPER(cyear)) = TRIM(UPPER(v_prospectCYear))
+		   AND TRIM(UPPER(color)) = TRIM(UPPER(v_prospectColor))
+		   AND TRIM(UPPER(trim)) = TRIM(UPPER(v_prospectTrim));
 
 	-- Create custom exception
 	ex_no_data_found EXCEPTION;
@@ -26,7 +37,7 @@ BEGIN
 	-- Get and store the number of prospect entries based on the prompted name
 	SELECT COUNT(*)
 	  INTO v_prospectCount
-	  FROM prospect
+	  FROM prospectCustomerCars
 	 WHERE TRIM(UPPER(cname)) = TRIM(UPPER('&p_customerName'));
 
 	-- Display Report Title
@@ -46,13 +57,13 @@ BEGIN
 		-- Loop through the results of the cursor and then display it to the console
 		LOOP
 			-- Fetch the results from the cursor into the prospect object variable
-			FETCH prospectiveCustomers_cur INTO v_prospect;
+			FETCH prospectiveCustomers_cur INTO v_prospectCName, v_prospectMake, v_prospectModel, v_prospectCYear, v_prospectColor, v_prospectTrim;
 
 			-- Exit the loop when there is nothing found in the cursor
 			EXIT WHEN prospectiveCustomers_cur%NOTFOUND;
 
 			-- Display prospective car information
-			DBMS_OUTPUT.PUT_LINE('Prospective Car: ' || TRIM(v_prospect.make) || ' ' || TRIM(v_prospect.model) || ' (' || TRIM(v_prospect.cyear) || ') ' || TRIM(v_prospect.color) || ' ' || TRIM(v_prospect.trim));
+			DBMS_OUTPUT.PUT_LINE('Prospective Car: ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectModel) || ' (' || TRIM(v_prospectCYear) || ') ' || TRIM(v_prospectColor) || ' ' || TRIM(v_prospectTrim));
 
 			-- Display section headding
 			DBMS_OUTPUT.PUT_LINE('Options: ');
