55babb35ca2e2c5f52a3e525b9098308774c761d
Bojoofu <graujsheridan@gmail.com>
2018-08-09 11:51:34 -0400

    Account for All Variations and Include new Format

--- sql/AllProspects.sql
+++ sql/AllProspects.sql
@@ -1,7 +1,12 @@
-SET SERVEROUTPUT ON;
+-- James Grau
+-- August 7, 2018
 
-VARIABLE g_options VARCHAR2(255);
+-- Set Serveroutput and format and then console linesize
+SET SERVEROUTPUT ON FORMAT WRAPPED;
+SET LINESIZE 132;
+SET FEEDBACK OFF;
 
+-- Declare needed variables and cursor
 DECLARE
 	-- Create needed variables to hold rows from tables and count of items
 	v_prospectCName prospect.cname%TYPE;
@@ -13,14 +18,15 @@ DECLARE
 	v_prospectOption options%ROWTYPE;
 	v_prospectCount NUMBER;
 	v_optionsCount NUMBER;
+	v_options VARCHAR(255) := '';
 
 	-- Cursor to store the prospect entries in the DB
-	CURSOR prospectiveCustomers_cur IS 
+	CURSOR c_prospectiveCustomers IS 
 		SELECT *
 		  FROM prospectCustomerCars;
 
 	-- Cursor to store the order options for the prospective entries
-	CURSOR prospectCustomersOptions_cur IS 
+	CURSOR c_prospectCustomersOptions IS 
 		SELECT o.*
 		  FROM prospect p
 		 INNER JOIN options o
@@ -42,17 +48,21 @@ BEGIN
 		   AND TRIM(UPPER(trim)) = TRIM(UPPER(v_prospectTrim));
 
 	-- Display Report Title & Headdings
-	DBMS_OUTPUT.PUT_LINE(CHR(9) || CHR(9) || CHR(9) || CHR(9) || CHR(9) || CHR(9) || CHR(9) || CHR(9) || CHR(9) || CHR(9) || 'Perspect List' || CHR(10) || CHR(10) || 'Name                 Want' || CHR(10) || '-------------------- --------------------------------------------------------------------------------');
+	DBMS_OUTPUT.PUT(LPAD('-', 131, '-'));
+	DBMS_OUTPUT.PUT_LINE(CENTERSTRING('Perspect List', 132));
+	DBMS_OUTPUT.PUT_LINE('');
+	DBMS_OUTPUT.PUT_LINE(RPAD('Name', 20) || ' ' || RPAD('Want', 110));
+	DBMS_OUTPUT.PUT_LINE(RPAD('-', 20, '-') || ' ' || RPAD('-', 110, '-'));
 
 	-- Open the prospectList cursor
-	OPEN prospectiveCustomers_cur;
+	OPEN c_prospectiveCustomers;
 		-- Loop through the results of the cursor and then display it to the console
 		LOOP
 			-- Fetch the results from the cursor into the prospect object variable
-			FETCH prospectiveCustomers_cur INTO v_prospectCName, v_prospectMake, v_prospectModel, v_prospectCYear, v_prospectColor, v_prospectTrim;
+			FETCH c_prospectiveCustomers INTO v_prospectCName, v_prospectMake, v_prospectModel, v_prospectCYear, v_prospectColor, v_prospectTrim;
 
 			-- Exit the loop when there is nothing found in the cursor
-			EXIT WHEN prospectiveCustomers_cur%NOTFOUND;
+			EXIT WHEN c_prospectiveCustomers%NOTFOUND;
 			
 			-- Get the count of options
 			SELECT COUNT(ocode)
@@ -68,58 +78,31 @@ BEGIN
 			-- Check if the prospect has any car options
 			IF (v_optionsCount > 0) THEN
 				-- Initialize options list variable
-				:g_options := 'w/ ';
+				v_options := 'w/ ';
 				
-				-- Open the prospectCustomersOptions_cur cursor
-				OPEN prospectCustomersOptions_cur;	
+				-- Open the c_prospectCustomersOptions cursor
+				OPEN c_prospectCustomersOptions;	
 					-- Loop through the results of the cursor and then display it to the console
 					LOOP
 						-- Fetch the results from the cursor into the prospect object variable
-						FETCH prospectCustomersOptions_cur INTO v_prospectOption;
+						FETCH c_prospectCustomersOptions INTO v_prospectOption;
 	
 						-- Exit the loop when there is nothing found in the cursor
-						EXIT WHEN prospectCustomersOptions_cur%NOTFOUND;					
+						EXIT WHEN c_prospectCustomersOptions%NOTFOUND;					
+						
 						-- Display options
-						:g_options := :g_options || TRIM(v_prospectOption.odesc) || '(' || TRIM(v_prospectOption.ocode) || ')';
+						v_options := v_options || TRIM(v_prospectOption.odesc) || '(' || TRIM(v_prospectOption.ocode) || ')';
 					-- End the loop
 					END LOOP;
 				-- Close the prospect List cursor
-				CLOSE prospectCustomersOptions_cur;
+				CLOSE c_prospectCustomersOptions;
 			END IF;
 
 			-- Display prospective car information
-			IF (v_prospectCYear IS NULL) THEN
-				IF (v_prospectColor IS NULL) THEN
-					IF (v_prospectModel IS NULL) THEN
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectTrim) || ' ' || :g_options);
-					ELSE
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectModel) || ' ' || TRIM(v_prospectTrim) || ' ' || :g_options);
-					END IF;	
-				ELSE
-					IF (v_prospectModel IS NULL) THEN
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectTrim) || ' ' || :g_options);
-					ELSE
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectColor) || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectModel) || ' ' || TRIM(v_prospectTrim));
-					END IF;	
-				END IF;
-			ELSE
-				IF (v_prospectColor IS NULL) THEN
-					IF (v_prospectModel IS NULL) THEN
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectCYear) || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectTrim) || ' ' || :g_options);
-					ELSE
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectCYear) || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectModel) || ' ' || TRIM(v_prospectTrim) || ' ' || :g_options);
-					END IF;	
-				ELSE
-					IF (v_prospectModel IS NULL) THEN
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectCYear) || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectTrim));
-					ELSE
-						DBMS_OUTPUT.PUT_LINE(v_prospectCName || ' ' || TRIM(v_prospectCYear) || ' ' || TRIM(v_prospectColor) || ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectModel) || ' ' || TRIM(v_prospectTrim));
-					END IF;	
-				END IF;
-			END IF;
+			DBMS_OUTPUT.PUT_LINE(RPAD(v_prospectCName, 20) || RPAD(regexp_replace(' ' || (TRIM(v_prospectCYear) || ' ' || TRIM(v_prospectColor)|| ' ' || TRIM(v_prospectMake) || ' ' || TRIM(v_prospectModel) || ' ' || TRIM(v_prospectTrim) || ' ' || v_options), ' +', ' '), 110));
 		END LOOP;
 
 	-- Close the prospect List cursor
-	CLOSE prospectiveCustomers_cur;
+	CLOSE c_prospectiveCustomers;
 END;
 /
